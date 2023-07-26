02f9f955e7df7ec4abd5afc2f5fa15d7f051c56b
hafizamirulhassan <hafizamirulhassan@gmail.com>
2023-07-26 05:41:34 +0500

    Third commit

--- src/PageObjectModel/Tests/conftest.py
+++ src/PageObjectModel/Tests/conftest.py
@@ -1,10 +1,7 @@
 import pytest
 from selenium import webdriver
-from selenium.webdriver.edge.options import Options as EdgeOptions
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.firefox.options import Options as FirefoxOptions
-from webdriver_manager.firefox import GeckoDriverManager
 from src.PageObjectModel.Config.config import TestData
 
 
@@ -15,27 +12,8 @@ def initialization_driver(request):
 
     if browser == "chrome":
         chrome_options = Options()
-        # chrome_options.add_argument("--headless")
         service = Service(TestData.CHROME_EXECUTABLE_PATH)
         web_driver = webdriver.Chrome(service=service, options=chrome_options)
-
-        # options = webdriver.ChromeOptions()
-        # web_driver = webdriver.Remote(
-        #     command_executor="http://192.168.10.3:4444/wd/hub",
-        #     options=options
-        # )
-
-    # if browser == "firefox":
-    #     # firefox_options = webdriver.FirefoxOptions()
-    #     #  # firefox_options.add_argument("--headless")
-    #     # service = Service(TestData.FIREFOX_EXECUTABLE_PATH)
-    #     # web_driver = webdriver.Firefox(service=service, options=firefox_options)
-    #
-    #     options = FirefoxOptions()
-    #     web_driver = webdriver.Remote(
-    #         command_executor="http://192.168.10.3:4444/wd/hub",
-    #         options=options
-    #     )
 
     request.cls.driver = web_driver
     web_driver.implicitly_wait(20)
